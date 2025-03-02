e459ea6958046678467b617f4c75e8c44cfda13a
Jessica Flores Olmos <jessica.flores309@myhunter.cuny.edu>
2025-03-02 16:19:59 -0500

    Initial Commit

---
+++ Oneiroi/OneiroiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneiroiApp.swift
+//  Oneiroi
+//
+//  Created by Jessica Flores Olmos on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct OneiroiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OneiroiTests/OneiroiTests.swift
@@ -0,0 +1,17 @@
+//
+//  OneiroiTests.swift
+//  OneiroiTests
+//
+//  Created by Jessica Flores Olmos on 3/2/25.
+//
+
+import Testing
+@testable import Oneiroi
+
+struct OneiroiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
