f25c751aa2b8ee17f3e6aa7aa1d83bd22177e0eb
Jessica Flores Olmos <jessica.flores309@myhunter.cuny.edu>
2025-03-02 19:28:14 -0500

    Implemented authorization

---
+++ Oneiroi-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Oneiroi-Bridging-Header.h
+//  Oneiroi
+//
+//  Created by Jessica Flores Olmos on 3/2/25.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
--- Oneiroi/OneiroiApp.swift
+++ Oneiroi/OneiroiApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct OneiroiApp: App {
+    @StateObject var spotifyController = SpotifyController()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL { url in
+                    spotifyController.setAccessToken(from: url)
+                }
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didFinishLaunchingNotification), perform: { _ in
+                    spotifyController.connect()
+                })
         }
     }
 }
